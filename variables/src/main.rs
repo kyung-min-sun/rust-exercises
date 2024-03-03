7465808b797b28668c32458f721fdadc20a7649e
kevinsun127 <kevinsun@usc.edu>
2024-03-03 01:03:00 -0500

    chapter 2 fin

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,28 +1,57 @@
-use std::io;
-
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
-
-    let a = [1, 2, 3, 4, 5];
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("failed to read line");
-
-    let index: usize = index.trim().parse().expect("not a number");
-
-    let element = a[index];
-
-    println!("{element} was chosen");
-
-    another_function();
+    fibonacci_number(9);
+    another_function(32, "unit")
 }
 
-fn another_function() {
-    println!("another function");
+fn fibonacci_number(n: i32) -> i32 {
+    let mut fib_0 = 0;
+    let mut fib_1 = 1;
+    if n == 0 {
+        return fib_0;
+    }
+    for _ in 1..n {
+        println!("{fib_1}");
+        let fib_2 = fib_0 + fib_1;
+        fib_0 = fib_1;
+        fib_1 = fib_2;
+    }
+    fib_1
+}
+
+fn another_function(x: i32, unit: &str) {
+    println!("value is {x} {unit}");
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("{y}");
+
+    let number = 6;
+
+    match number {
+        i32::MIN..=0 => println!("test"),
+        1 => println!("hi"),
+        2..=i32::MAX => println!("test"),
+    }
+
+    let number = if true { 5 } else { 6 };
+    println!("{number}");
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    const A: [i32; 4] = [10, 20, 30, 40];
+    for index in 1..A.len() {
+        println!("{index}")
+    }
+
+    println!("{result}");
 }
