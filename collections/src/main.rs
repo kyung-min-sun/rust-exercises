939e63f5b998f1bc51b7be881d724f19c27bf4fc
kevinsun127 <kevinsun@usc.edu>
2024-03-10 22:57:21 -0400

    vectors

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,9 +1,16 @@
+use std::collections::HashMap;
+
 fn main() {
     let v: Vec<i32> = Vec::new();
+    let v2 = vec![0];
     let mut k = match v.get(10) {
         Some(value) => value,
         None => &0,
     };
     k = &10;
-    println!("{k}")
+    println!("{k}");
+
+    let mut map = HashMap::new();
+    map.insert(String::from("hello"), String::from("world"));
+    map.get("hello");
 }
